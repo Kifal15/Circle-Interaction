45edfd8846d6dd62fd3735636e24e27754da0329
Zalkifal Khan <88394912+Kifal15@users.noreply.github.com>
2023-04-21 18:13:01 +0500

    Update Readpayouts.js

--- src/Components/Readpayouts.js
+++ src/Components/Readpayouts.js
@@ -30,19 +30,25 @@ function PayoutsList() {
     <div>
       <h1>List of Payouts Made Thru Circle</h1>
       <div style={{ height: '500px', overflowY: 'scroll' }}>
-        <ul>
-          {payouts.map(payout => (
-            <li key={payout.id}>
-              <p>Source Wallet ID: {payout.sourceWalletId}</p>
-              <p>Destination ID: {payout.destination.id}</p>
-              <p>Destination Name: {payout.destination.name}</p>
-              <p>Destination Type: {payout.destination.type}</p>
-              <p>Amount: {payout.amount.amount} {payout.amount.currency}</p>
-              <p>Fees: {payout.fees.amount} {payout.fees.currency}</p>
-              <p>Status: {payout.status}</p>
-            </li>
-          ))}
-        </ul>
+        {payouts.length > 0 ? (
+          <ul>
+            {payouts.map(payout => (
+              <li key={payout.id}>
+                <p>Source Wallet ID: {payout.sourceWalletId}</p>
+                <p>Destination ID: {payout.destination.id}</p>
+                <p>Destination Name: {payout.destination.name}</p>
+                <p>Destination Type: {payout.destination.type}</p>
+                <p>Amount: {payout.amount.amount} {payout.amount.currency}</p>
+                {payout.fees && (
+                  <p>Fees: {payout.fees.amount} {payout.fees.currency}</p>
+                )}
+                <p>Status: {payout.status}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No payouts found</p>
+        )}
       </div>
     </div>
   );
